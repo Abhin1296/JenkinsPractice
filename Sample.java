fa6161c5ff790348cb369dcafd92c265bb8034ae
Abhin1296 <110671112+Abhin1296@users.noreply.github.com>
2023-06-03 12:47:31 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,12 @@
+public class HelloGuys{
+  
+  public static void main(String[] args)
+  {
+    System.out.println("Hello A");
+    System.out.println("Hello B");
+    System.out.println("Hello C");
+    System.out.println("Hello D");
+    
+  }
+  
+}
